45736506a6e68e4a1d64dd483c116b99edd98375
Koki Fukuda <ko.fu.dev@gmail.com>
2023-06-12 23:19:03 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use num::FromPrimitive;
 use reqwest::blocking::Client;
+use std::collections::HashMap;
 use std::error::Error;
 use std::io::prelude::*;
 use std::ops::Range;
@@ -217,6 +218,7 @@ enum AttributeValue {
     NaturalLanguage(String),
     MimeMediaType(String),
     MemberAttrName(String),
+    CollectionAttribute(HashMap<String, AttributeValue>),
 }
 
 macro_rules! write_int_be {
@@ -250,16 +252,16 @@ macro_rules! write_attr {
 
 macro_rules! read_and_decode {
     ($reader:ident,String) => {{
-        let len = read_and_decode!($reader, i16)?;
+        let len = read_and_decode!($reader, i16).unwrap();
         let mut buf = vec![0u8; len as usize];
-        $reader.read_exact(buf.as_mut_slice())?;
+        $reader.read_exact(buf.as_mut_slice()).unwrap();
         String::from_utf8(buf)
     }};
 
     ($reader:ident,AttributeValue::$attr_type:expr) => {{
-        let len = read_and_decode!($reader, i16)?;
+        let len = read_and_decode!($reader, i16).unwrap();
         let mut buf = vec![0u8; len as usize];
-        $reader.read_exact(buf.as_mut_slice())?;
+        $reader.read_exact(buf.as_mut_slice()).unwrap();
         match $attr_type {
             AttributeSyntax::Integer => {
                 if len == 4 {
@@ -355,6 +357,52 @@ macro_rules! read_and_decode {
     }};
 }
 
+fn parse_collection_attribute(
+    reader: &mut dyn Read,
+) -> Result<HashMap<String, AttributeValue>, ()> {
+    let mut result = HashMap::<String, AttributeValue>::new();
+
+    loop {
+        let value_tag: AttributeSyntax =
+            FromPrimitive::from_i8(read_and_decode!(reader, i8).unwrap()).unwrap();
+        if let AttributeSyntax::MemberAttrName | AttributeSyntax::EndCollection = value_tag {
+            read_and_decode!(reader, String).unwrap();
+
+            match read_and_decode!(reader, AttributeValue::value_tag) {
+                Ok(AttributeValue::MemberAttrName(attr_name)) => {
+                    let value_tag: AttributeSyntax =
+                        FromPrimitive::from_i8(read_and_decode!(reader, i8).unwrap()).unwrap();
+                    if let AttributeSyntax::EndCollection = value_tag {
+                        return Err(());
+                    }
+                    match read_and_decode!(reader, AttributeValue::value_tag) {
+                        Ok(AttributeValue::BegCollection) => {
+                            let collection_attr = parse_collection_attribute(reader).unwrap();
+                            result.insert(
+                                attr_name,
+                                AttributeValue::CollectionAttribute(collection_attr),
+                            );
+                        }
+                        Ok(attr_value) => {
+                            result.insert(attr_name, attr_value);
+                        }
+                        Err(_) => return Err(()),
+                    }
+                }
+                Ok(AttributeValue::EndCollection) => {
+                    break;
+                }
+                Ok(_) => return Err(()),
+                Err(_) => return Err(()),
+            }
+        } else {
+            return Err(());
+        }
+    }
+
+    Ok(result)
+}
+
 fn parse_response(data: Vec<u8>) -> Result<(), Box<dyn Error>> {
     let mut data = &data[..];
 
@@ -370,6 +418,9 @@ fn parse_response(data: Vec<u8>) -> Result<(), Box<dyn Error>> {
 
     let mut begin_attribute_group_tag: DelimiterTag =
         FromPrimitive::from_i8(read_and_decode!(data, i8)?).unwrap();
+
+    let mut attributes = Vec::<AttributeValue>::new();
+
     loop {
         println!("begin-attribute-group-tag: {:?}", begin_attribute_group_tag);
         match begin_attribute_group_tag {
@@ -392,9 +443,17 @@ fn parse_response(data: Vec<u8>) -> Result<(), Box<dyn Error>> {
 
             match read_and_decode!(data, AttributeValue::value_tag) {
                 Ok(AttributeValue::Unsupported(value)) => {
-                    println!("value: {:?} (type: {:?})", value, value_tag)
+                    println!("value: {:?} (type: {:?})", value, value_tag);
                 }
-                Ok(value) => println!("value: {:?}", value),
+                Ok(AttributeValue::BegCollection) => {
+                    let collection_attr = parse_collection_attribute(&mut data).unwrap();
+                    println!("collection value: {:?}", collection_attr);
+                    attributes.push(AttributeValue::CollectionAttribute(collection_attr));
+                }
+                Ok(value) => {
+                    println!("value: {:?}", value);
+                    attributes.push(value);
+                }
                 Err(_) => println!("value: ({:?})<binary data>", value_tag),
             }
         }
@@ -404,7 +463,8 @@ fn parse_response(data: Vec<u8>) -> Result<(), Box<dyn Error>> {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let printer_addr = std::env::var("PRINTER_ADDR").expect("PRINTER_ADDR is not set (should be a value like \"192.0.2.1:631\")");
+    let printer_addr = std::env::var("PRINTER_ADDR")
+        .expect("PRINTER_ADDR is not set (should be a value like \"192.0.2.1:631\")");
 
     let client = Client::new();
 
