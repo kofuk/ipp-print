d2cb7c2f62057795bf520356f26792e2ac49c822
Koki Fukuda <ko.fu.dev@gmail.com>
2023-06-24 23:51:14 +0900

    wip

--- src/pwgraster.rs
+++ src/pwgraster.rs
@@ -1,13 +1,83 @@
 use std::error::Error;
 use std::io::prelude::*;
 
-pub fn read_raster<R>(reader: &mut R) -> Result<(), Box<dyn Error>> where R: Read {
+// Heavily commented based on CUPS Raster Format documentation.
+// It may be inaccurate.
+
+struct PWGRaster {
+    /// Ras2 if big-endian, 2SaR if little endian
+    sync_word: [u8; 4],
+    /// NUL-terminated string saying "PwgRaster".
+    pwg_raster: [u8; 64],
+    /// NUL-terminated string indicating media color name.
+    /// Empty string for default value.
+    media_color: [u8; 64],
+    /// NUL-terminated string indicating media type name.
+    /// Empty string for default value.
+    media_type: [u8; 64],
+    print_content_optimize: [u8; 64],
+    reserved_0: [u8; 12],
+    /// (Maybe) when to cut paper.
+    /// 0: Never cut media
+    /// 1: Cut roll after file
+    /// 2: Cut roll after job
+    /// 3: Cut roll after set
+    /// 4: Cut roll after page
+    /// For plain papers, it should be ok to leave this 0.
+    cut_media: u32,
+    /// 0: Single-sided
+    /// 1: Double-sided
+    duplex: u32,
+    /// [0]: Horizontal dpi
+    /// [1]: Vertical dpi
+    hw_resolution: [u32; 2],
+    reserved_1: [u8; 16],
+    /// 0: Do not insert separator sheets
+    /// 1: Insert separator sheets
+    ///
+    /// ???  What "separator sheets" means? Simply insert paper which is not printed, or hardware-specific "separator" exists?
+    insert_sheet: u32,
+    /// 0: Do not jog pages
+    /// 1: Jog pages after file
+    /// 2: Jog pages after job
+    /// 3: Jog pages after set
+    ///
+    /// ???  What is "Jog"? To wait for ink to dry?
+    jog: u32,
+    /// 0: Top edge is first
+    /// 1: Right edge is first
+    /// 2: Bottom edge is first
+    /// 3: Left edge is first
+    ///
+    /// ???  What this parameter means? Direction of printing?
+    leading_edge: u32,
+    reserved_2: [u8; 12],
+    /// Input slot position from 0 to N
+    ///
+    /// ???  What this parameter means? Place the paper is set?
+    media_position: u32,
+    /// Media weight in g/m^2.
+    /// 0 for default.
+    media_weight_metric: u32,
+    reserved_3: [u8; 8],
+    /// The number of sheets to print
+    /// 0 for printer default.
+    num_copies: u32,
+}
+
+pub fn read_raster<R>(reader: &mut R) -> Result<(), Box<dyn Error>>
+where
+    R: Read,
+{
     let mut n_read = 0;
 
     let mut buf = [0u8; 4];
     n_read += buf.len();
     reader.read_exact(&mut buf)?;
-    println!("Synchronization Word={}", String::from_utf8(buf.to_vec()).unwrap());
+    println!(
+        "Synchronization Word={}",
+        String::from_utf8(buf.to_vec()).unwrap()
+    );
 
     let mut buf = [0u8; 64];
     reader.read_exact(&mut buf)?;
@@ -28,7 +98,10 @@ pub fn read_raster<R>(reader: &mut R) -> Result<(), Box<dyn Error>> where R: Rea
     let mut buf = [0u8; 64];
     reader.read_exact(&mut buf)?;
     n_read += buf.len();
-    println!("PrintContentOptinize={}", String::from_utf8(buf.to_vec()).unwrap());
+    println!(
+        "PrintContentOptimize={}",
+        String::from_utf8(buf.to_vec()).unwrap()
+    );
 
     let mut buf = [0u8; 4];
     reader.read_exact(&mut buf)?;
@@ -114,7 +187,7 @@ pub fn read_raster<R>(reader: &mut R) -> Result<(), Box<dyn Error>> where R: Rea
     let mut buf = [0u8; 4];
     reader.read_exact(&mut buf)?;
     n_read += buf.len();
-    println!("(cups)ManualFeed[r]={}", u32::from_be_bytes(buf));
+    println!("(cups)ManualFeed={}", u32::from_be_bytes(buf));
 
     let mut buf = [0u8; 4];
     reader.read_exact(&mut buf)?;
@@ -350,7 +423,10 @@ pub fn read_raster<R>(reader: &mut R) -> Result<(), Box<dyn Error>> where R: Rea
     let mut buf = [0u8; 64];
     reader.read_exact(&mut buf)?;
     n_read += buf.len();
-    println!("PageSizeName={:?}", String::from_utf8(buf.to_vec()).unwrap());
+    println!(
+        "PageSizeName={:?}",
+        String::from_utf8(buf.to_vec()).unwrap()
+    );
 
     println!("{}", n_read);
 
